61226462ccb3c1c3932694fd212f1da6e0669956
Abulkosim <abulkasimkattaev@gmail.com>
2024-10-07 13:43:24 +0500

    refactor: calculating hash for a file

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
+const fs = require('fs');
+const crypto = require('crypto');
+const path = require('path');
+
 const calculateHash = async () => {
-    // Write your code here 
+    const filePath = path.join(__dirname, 'files/fileToCalculateHashFor.txt');
+    const hash = crypto.createHash('sha256');
+
+    try {
+        const stream = fs.createReadStream(filePath);
+
+        for await (const chunk of stream) {
+            hash.update(chunk);
+        }
+
+        const fileHash = hash.digest('hex');
+        console.log(`SHA256 hash for file: \n${fileHash}`);
+    } catch (error) {
+        console.error(error);
+    }
 };
 
-await calculateHash();
+calculateHash();
