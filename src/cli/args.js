652171152d0f79dec7831311158cee89415c82d7
Abulkosim <abulkasimkattaev@gmail.com>
2024-10-07 12:54:03 +0500

    refactor: args & env functions

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    const parsedArgs = {};
+
+    for (let i = 0; i < args.length; i += 2) {
+        const propName = args[i].replace(/^--/, '');
+        const value = args[i + 1];
+        parsedArgs[propName] = value;
+    }
+
+    Object.entries(parsedArgs).forEach(([propName, value]) => {
+        console.log(`${propName} is ${value}`);
+    });
 };
 
 parseArgs();
